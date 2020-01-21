23e263b950c55b8a281ec74646f79eb6fb664f59
mikeyv95 <michaelwvaldez@gmail.com>
2020-01-21 13:03:15 -0800

    merged original file with JS script file

--- js/script.js
+++ js/script.js
@@ -2,32 +2,70 @@
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
+/*start off the Random Quote project with quotes from The Office using objects within an array*/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+var officeQuotes = [
+	{	quote : '"Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way."',
+		source : "Michael Scott",
+        episode : "Season 5, The Duel",
+        year : "2008"
+    },
 
-/*** 
- * `quotes` array 
-***/
+	{	quote : "Yes",
+		source : "Michael Scott",
+        episode : "Season 5, Stress Relief: Part 1",
+        year : "2008"
+    },
+
+   
+    {
+        quote : "I am Beyoncé, always.",
+        source : "Michael Scott", 
+        episode : "Season 6, The Chump",
+        year : "2009"
+    },
+     
+    { 
+        quote : "Did I stutter?", 
+        source : "Stanley Hudson", 
+        episode : "Season 4, Did I stutter?",
+        year : "2007"
+    },
+
+    {
+        quote : "Fact: Bears eat beets. Bears. Beets. Battlestar Galactica.", 
+        source : "Jim Halpert",
+        episode : "Season 3, Product Recall",
+        year : "2007"
+    } 
+  ];
+
+/* the getRandomQuote function will pull a random quote from the array above*/
+
+function getRandomQuote() {
+    var randomQuote = Math.floor(Math.random() * officeQuotes.length);
+    return officeQuotes[randomQuote];
+    document.getElementById('quote-box').innerHTML = officeQuotes[randomQuote];
+}
+
+/* the printQuote function will print the above selected random quote*/
+
+function printQuote() {
+    var result = getRandomQuote();
+    var message = ' ';
+    message  = '<p class = "quote">' + officeQuotes.quote + '</p>';
+    message += '<p class = "source">' + officeQuotes.source + '</p>';
+    
+    if (officeQuotes.citation) {
+        message += '<p class="citation">' + officeQuotes.citation + '</p>';
+      }
+
+    if (officeQuotes.year) {
+        message += '<p class="year">' + officeQuotes.year + '</p>';
+      }
+}
 
 
-
-/***
- * `getRandomQuote` function
-***/
-
-
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+printQuote();
